060fa9a1c48b293089107500dc0b7925e495a793
pamelon <glonowiec@gmail.com>
2022-03-21 23:16:09 +0100

    Adding gameplay

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import argparse
 from strategy import Basic
 
-def begin_game():
+def begin_game(strategy):
     print("Cześć, witaj w grze w zmazywanie repetycji!")
 
     alf_length = int(input("Jak długi chcesz wybrać alfabet do rozgrywki?\n"))
@@ -18,14 +18,25 @@ def begin_game():
     print("Gramy wielkimi literami A-{}, na słowo maksymalnej długości {}, na {} rund. \n"\
         .format(last_letter, word_length, round_length))
 
-    return (alf_length, word_length, round_length)
+    return run_game(alf_length, word_length, round_length)
+
+#True - wygraliśmy my, False - wygrał komputer
+def run_game(alf_length, word_length, round_length):
+    return True
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument('-s', '--strategy', dest='strategy', type=int, default=0,
                         help='chosen strategy for the computer player')
-    (alf_length, word_length, round_length) = begin_game()
+    args = vars(parser.parse_args())
+
+    result = begin_game(args['strategy'])
+
+    if(result):
+        print("Brawo wygrałaś!")
+    else:
+        print("Wygrał przeciwnik. Następnym razem na pewno pójdzie Ci lepiej :)")
 
     
 main()
